ff60324880c0dd7e6c04c3dc3267e20001cb47d0
dhirendravsingh <dhirendravsingh09@gmail.com>
2025-07-07 18:46:55 +0530

    basic webhook server structured

--- webhook/src/index.ts
+++ webhook/src/index.ts
@@ -1,10 +1,28 @@
 import express from "express"
-import { PrismaClient } from "@prisma/client"
+import { PrismaClient, Prisma } from "@prisma/client"
 
-const prisma = new PrismaClient()
+const client = new PrismaClient()
 const app = express()
 
-app.get("/webhook", (req, res) => { 
-    res.send("Webhook is active")
+app.post("/hooks/catch/:userId/:zapId", async (req, res) => { 
+    const userId = req.params.userId
+    const zapId = req.params.zapId
+    const metaData = req.body
+    
+    await client.$transaction(async (tx : Prisma.TransactionClient) => {
+        const run = await client.zaprun.create({
+            data : {
+                zapid : zapId,
+                metadata : metaData
+            }
+        })
+
+        await client.zaprunOutbox.create({
+            data : {
+                zaprunId : run.id
+            }
+        })
+    })
+
 })
 
