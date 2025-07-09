5f118d58b6844f327b118f9e7c74b6a44647da87
dhirendravsingh <dhirendravsingh09@gmail.com>
2025-07-09 11:25:58 +0530

    removed the dist folder

--- primary-backend/dist/db/index.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const prisma_1 = require("../generated/prisma");
-const client = new prisma_1.PrismaClient;
-exports.default = client;
